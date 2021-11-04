ab3404c633208a81aa9f5d5179b920729764bb5f
delpotro96 <freeone1224@gmail.com>
2021-11-04 20:43:18 +0900

    Add BoardPratice

---
+++ src/test/java/com/example/boardpratice/BoardPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
